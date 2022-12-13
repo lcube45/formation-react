3a3ef3dcc88567879a361c6d93834a161271664e
Laurent LE LARDEUX <laurent.le-lardeux@inetum.com>
2022-12-13 14:44:56 +0100

    example global css styles

--- got/src/layout.tsx
+++ got/src/layout.tsx
@@ -3,6 +3,7 @@ import { Link, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home';
 import Person from './pages/person';
 import PersonsPage from './pages/persons';
+import './style.css'
 
 interface LayoutProps {
 
@@ -18,7 +19,7 @@ const Detail: React.FC<LayoutProps> = (props: LayoutProps) => {
                 <Route path="/person/:id" element={<Person />} />
             </Routes>
         </header>
-        <footer style={{background: "red", padding: "10px"}}>footer</footer>
+        <footer className='my-footer'>footer</footer>
         </>
     )
 }
